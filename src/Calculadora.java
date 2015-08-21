111d498cbdc029fba8a10fe7182c069e4e7b1fa4
aju13171 <c.valentinaju@gmail.com>
2015-08-20 22:47:04 -0600

    error en las operaciones con las listas

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -37,13 +37,16 @@ public class Calculadora {
 		FactoryAV<String> factoryAV = new FactoryAV<String>();
 		FactoryListas<String> factoryListas = new FactoryListas<String>();
 		
+		//para los valores
+		int val1 = 0;
+		int val2 = 0;
 		/**
 		 * objetos que van a contener las listas de cualquier tipo
 		 * operandos: guardan solo los operandos leidos en tipo int
 		 * operadores: guardan solo operadores en tipo string
 		 */
-		Pila lista = null;
-		Listas lista2 = null;
+		Pila<String> lista = null;
+		Listas<String> lista2 = null;
 		//contiene cuantos operandos y operadores se ingresan
 		int size = 0;
 		//contiene la linea leida
@@ -134,11 +137,13 @@ public class Calculadora {
 		 * lectura caracter por caracter
 		 * se eliminan espacios en blanco
 		 */
-		
+		//reinicio de size
+		size = 0;
 		for (int flag = 0; flag<bite.length(); flag++){
 			if (!(Character.toString(bite.charAt(flag)).equals(" "))){
 				System.out.println(bite.charAt(flag));
 				temp = temp+Character.toString(bite.charAt(flag));
+				size++;
 			}
 		}
 		linea = temp;
@@ -154,6 +159,48 @@ public class Calculadora {
 			lista2 = factoryListas.setlista(tipo);
 		}
 		
-		
+		//ingreso de los datos y calculos
+		if (lista == null){
+			//es tipo listas
+			//Ingreso de los datos en la lista
+			for(int i=0; i<(size-1); i++){
+				temp = Character.toString(linea.charAt(i));
+				System.out.println("ingreso: " + temp);
+				lista2.addFirst(temp);
+			}
+			System.out.println("fin de ingresos\n\n ");
+			//calculos
+			while(size>1){
+				System.out.println(size);
+				val1 = Integer.parseInt(lista2.removeLast());
+				val2 = Integer.parseInt(lista2.removeLast());
+				System.out.println("valor 1: " + val1);
+				System.out.println("valor 2: " + val2);
+				if ((lista2.removeLast()) == "+"){
+					val1 = val1 + val2;
+					System.out.println("suma= " + val1);
+					//se vuelve a ingresar el resultado
+					lista2.addLast(Integer.toString(val1));
+					//solo se restan 2 porque se vuelve a ingresar el resultado
+					size = size - 2;
+				}
+				if ((lista2.removeLast()) == "*"){
+					val1 = val1 * val2;
+					System.out.println("multiplicacion= " + val1);
+					//se vuelve a ingresar el resultado
+					lista2.addLast(Integer.toString(val1));
+					//solo se restan 2 porque se vuelve a ingresar el resultado
+					size = size - 2;
+				}
+			}
+		}
+		if (lista2 == null){
+			//es tipo AV
+			for(int i=0; i<(size-1); i++){
+				temp = Character.toString(linea.charAt(i));
+				System.out.println("ingreso: " + temp);
+				lista.push(temp);
+			}
+		}
 	}
 }
